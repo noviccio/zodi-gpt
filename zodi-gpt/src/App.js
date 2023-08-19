87addbc64767d355ce799bba9023435401fd3d86
noviccio <mnovicio2001@gmail.com>
2023-08-19 18:55:02 -0400

    Modified CSS

--- zodi-gpt/src/App.js
+++ zodi-gpt/src/App.js
@@ -4,12 +4,29 @@ const App = () => {
     <div className="App">
       <section className="side-bar">
         <button>+ New chat</button>
-        <ul className="history"></ul>
+        <ul className="history">
+           <li>test</li>
+        </ul>
         <nav>
           <p>Made by Michael Novicio</p>
         </nav>
       </section>
-      <section className="main"></section>
+      <section className="main">
+        <h1>ZodiGPT</h1>
+        <ul className="feed">
+
+        </ul>
+        <div className ="bottom-section">
+            <div className="input-container">
+              <input/>
+              <div id="submit">➢</div>
+            </div>
+            <p className= "info">
+              ZodiGPT Aug 19 Version. This is a project made to 
+              understand how the OpenAPI works!
+            </p>
+        </div>
+      </section>
     </div>
   )
 }
