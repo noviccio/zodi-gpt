9acaf4464fe45572b4fc44d0f0bcca7b6635d96f
noviccio <mnovicio2001@gmail.com>
2023-08-19 18:03:37 -0400

    set up react app

---
+++ zodi-gpt/src/App.js
@@ -0,0 +1,17 @@
+
+const App = () => {
+  return (
+    <div className="App">
+      <section className="side-bar">
+        <button>+ New chat</button>
+        <ul className="history"></ul>
+        <nav>
+          <p>Made by Michael Novicio</p>
+        </nav>
+      </section>
+      <section className="main"></section>
+    </div>
+  )
+}
+
+export default App;
