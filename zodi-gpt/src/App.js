7e6edb435601a846ce928073c533aa329b5b10fa
noviccio <mnovicio2001@gmail.com>
2023-08-25 18:29:45 -0400

    changed placeholder values with new variables

--- zodi-gpt/src/App.js
+++ zodi-gpt/src/App.js
@@ -1,23 +1,30 @@
+import { useState, useEffect} from 'react'
+
+
 
 const App = () => { //creates an app function that returns HTML
+
+  const [value, setValue] = useState(null)
+  const [ message, setMessage] = useState(null)
 
   const getMessages = async () => {
 
     const options = {
       method: "POST",
       body : JSON.stringify({
-        message: "hello how are you?"
+        message: value
       }), 
       headers: {
           "Content-Type": "application/json"
       }
-    };
+    }
 
 
     try{
         const response = await fetch('http://localhost:8000/completions', options)
         const data = await response.json()
         console.log(data)
+        setMessage(data.choices[0].message)
     }catch (error) {
        console.error(error)
     }
@@ -45,7 +52,7 @@ const App = () => { //creates an app function that returns HTML
         </ul>
         <div className ="bottom-section">
             <div className="input-container">
-              <input/>
+              <input value = {value} onChange={(e) => setValue(e.target.value)}/>
               <div id="submit" onClick={getMessages}>➢</div>
             </div>
             <p className= "info">
